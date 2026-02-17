2b22136921cfbfd46202dfce79f0f72d8f083ba1
Mayuresh-22 <mayureshchoudhary22@gmail.com>
2026-02-18 00:12:47 +0530

    Add ConfigService for managing configuration loading, saving, and updating

---
+++ src/config/config.py
@@ -0,0 +1,33 @@
+import os
+from os.path import dirname as up
+
+from src.types.config import ConfigFileType
+
+
+CONFIG_PATH = os.path.join(up(up(up(__file__))), ".openarc/config.json")
+
+if not os.path.exists(CONFIG_PATH):
+    os.makedirs(os.path.dirname(CONFIG_PATH), exist_ok=True)
+    with open(CONFIG_PATH, "w") as f:
+        f.write("{}")
+
+
+class ConfigService:
+    def load_config(self) -> ConfigFileType:
+        import json
+
+        with open(CONFIG_PATH, "r") as f:
+            self.config_file = ConfigFileType(**json.load(f))
+        return self.config_file
+
+    def save_config(self):
+        import json
+
+        with open(CONFIG_PATH, "w") as f:
+            json.dump(self.config_file.model_dump(), f, indent=4)
+
+    def update_config(self, new_config: dict):
+        self.config_file = self.load_config()
+        for key, value in new_config.items():
+            setattr(self.config_file, key, value)
+        self.save_config()
