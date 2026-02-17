115b85744782b8e3500f045356a93f56713a650c
Mayuresh-22 <mayureshchoudhary22@gmail.com>
2026-02-18 00:11:11 +0530

    Refactor config types

--- src/types/config.py
+++ src/types/config.py
@@ -1,20 +1,30 @@
+from typing import Optional
 from pydantic import BaseModel
 from os.path import dirname as up
 import os
 
 
-class SupportedModel(BaseModel):
+class SupportedLLMProvider(BaseModel):
     code_name: str
     name: str
 
 
-class AvailableModel(BaseModel):
+class AvailableLLMProvider(BaseModel):
     provider_name: str
+    code_name: str
     model_id: str
     api_key: str
 
 
-class ConfigType(BaseModel):
-    supported_models: list[SupportedModel] = []
-    available_models: list[AvailableModel] = []
+class CurrentLLMProvider(BaseModel):
+    provider_name: str
+    code_name: str
+    model_id: str
+
+
+class ConfigFileType(BaseModel):
+    available_llm_providers: list[AvailableLLMProvider]
+    available_llm_providers_by_code_name: list[tuple[str, str]]
+    current_llm_provider: Optional[AvailableLLMProvider]
     root_dir: str = os.path.join(up(up(up(__file__))))
+    supported_llm_providers: list[SupportedLLMProvider]
