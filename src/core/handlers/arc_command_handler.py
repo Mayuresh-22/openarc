030aa66a0bec294f8057f97c8697876662f5592d
Mayuresh-22 <mayureshchoudhary22@gmail.com>
2026-02-19 13:15:30 +0530

    Refactor ArcCommandHandler: Update command handling to use ConfigMenuOptionsValue and add handle sys prompt modification option

--- src/core/handlers/arc_command_handler.py
+++ src/core/handlers/arc_command_handler.py
@@ -1,14 +1,12 @@
 from typing import Callable
 
-from prompt_toolkit import HTML, choice, prompt
-from prompt_toolkit.filters import is_done
+from src.core.prompts.prompt import PromptService
+from src.const.config import ConfigMenuOptionsValue
 from src.llm.llm_provider_registry import LLMProviderRegistry
-from src.config.config import ConfigService
 from src.core.ui.config_menu_ui import ConfigMenuUI
-from src.config.llm import SUPPORTED_MODEL_PROVIDERS
 from src.core.handlers.base_handler import BaseHandler
 from src.types.cli import CLIOutput
-from src.types.config import AvailableLLMProvider, CurrentLLMProvider
+from src.types.config import AvailableLLMProvider
 
 
 class ArcCommandHandler(BaseHandler):
@@ -16,6 +14,7 @@ class ArcCommandHandler(BaseHandler):
         self,
         config_menu_ui_handler: ConfigMenuUI,
         llm_provider_registry: LLMProviderRegistry,
+        prompt_service: PromptService
     ):
         super().__init__()
         self.valid_arc_commands_map: dict[str, Callable] = {
@@ -24,6 +23,7 @@ class ArcCommandHandler(BaseHandler):
         }
         self.config_menu_ui_handler = config_menu_ui_handler
         self.llm_provider_registry = llm_provider_registry
+        self.prompt_service = prompt_service
 
     def handle(self, content: list[str]) -> CLIOutput:
         if content[0] not in self.valid_arc_commands_map:
@@ -39,7 +39,7 @@ class ArcCommandHandler(BaseHandler):
         print()
 
         match result:
-            case "1":
+            case ConfigMenuOptionsValue.OPTION_ADD_PROVIDER.value:
                 llm_provider, api_key, model_id = (
                     self.config_menu_ui_handler.handle_add_llm_provider()
                 )
@@ -67,7 +67,7 @@ class ArcCommandHandler(BaseHandler):
                     model_id=model_id,  # type: ignore
                     api_key=api_key,  # type: ignore
                 )
-                self.llm_provider_registry.register_provider(new_provider)
+                self.llm_provider_registry.register_llm_provider(new_provider)
 
                 return CLIOutput(
                     stdout=f"Added LLM provider: {llm_provider.name} with model ID {model_id}",  # type: ignore
@@ -75,7 +75,7 @@ class ArcCommandHandler(BaseHandler):
                     exitcode=0,
                 )
 
-            case "2":
+            case ConfigMenuOptionsValue.OPTION_SWITCH_PROVIDER.value:
                 llm_provider = self.config_menu_ui_handler.handle_switch_llm_provider(
                     self.llm_provider_registry.get_available_llm_providers()
                 )
@@ -87,14 +87,27 @@ class ArcCommandHandler(BaseHandler):
                         exitcode=0,
                     )
 
-                self.llm_provider_registry.set_current_provider(llm_provider)
+                self.llm_provider_registry.set_current_llm_provider(llm_provider)
 
                 return CLIOutput(
                     stdout=f"Switched LLM Provider to: {llm_provider.provider_name} ({llm_provider.model_id})",  # type: ignore
                     stderr=None,
                     exitcode=0,
                 )
-            case "3":
+            
+            case ConfigMenuOptionsValue.OPTION_MOD_SYS_PROMPT.value:
+                new_sys_prompt = self.config_menu_ui_handler.handle_mod_sys_prompt(
+                    current_sys_prompt=self.prompt_service.get_sys_prompt()
+                )
+                self.prompt_service.set_sys_prompt(new_sys_prompt)
+
+                return CLIOutput(
+                    stdout="System prompt updated successfully.",
+                    stderr=None,
+                    exitcode=0,
+                )
+            
+            case ConfigMenuOptionsValue.OPTION_CANCEL.value:
                 return CLIOutput(
                     stdout="Returning to Main Menu.", stderr=None, exitcode=0
                 )
