1f789252c306c8abe97bfdd39b4c96dcbb6180ee
Mayuresh-22 <mayureshchoudhary22@gmail.com>
2026-02-19 13:13:38 +0530

    Refactor LLMProviderRegistry: Rename methods for consistency and save current provider in class property

--- src/llm/llm_provider_registry.py
+++ src/llm/llm_provider_registry.py
@@ -1,12 +1,12 @@
-from git import Optional
+from typing import Optional
 from src.config.config import ConfigService
 from src.types.config import AvailableLLMProvider, SupportedLLMProvider
 
 
 class LLMProviderRegistry:
     def __init__(self, config_service: ConfigService):
-        self.registry = {}
         self.config_service = config_service
+        self.current_llm_provider = self.config_service.load_config().current_llm_provider
 
     def is_provider_registered(self, code_name: str, model_id: str) -> bool:
         for (
@@ -17,7 +17,7 @@ class LLMProviderRegistry:
                 return True
         return False
 
-    def register_provider(self, llm_provider: AvailableLLMProvider):
+    def register_llm_provider(self, llm_provider: AvailableLLMProvider):
         temp_config_file = self.config_service.load_config()
 
         temp_config_file.available_llm_providers.append(
@@ -35,25 +35,23 @@ class LLMProviderRegistry:
         self.config_service.save_config()
 
         if not temp_config_file.current_llm_provider:
-            self.set_current_provider(llm_provider)
+            self.set_current_llm_provider(llm_provider)
 
-    def set_current_provider(self, llm_provider: AvailableLLMProvider):
+    def set_current_llm_provider(self, llm_provider: AvailableLLMProvider):
         temp_config_file = self.config_service.load_config()
 
-        temp_config_file.current_llm_provider = AvailableLLMProvider(
+        self.current_llm_provider = AvailableLLMProvider(
             provider_name=llm_provider.provider_name,  # type: ignore
             code_name=llm_provider.code_name,  # type: ignore
             model_id=llm_provider.model_id,  # type: ignore
             api_key=llm_provider.api_key,  # type: ignore
         )
+        temp_config_file.current_llm_provider = self.current_llm_provider
 
         self.config_service.save_config()
 
-    def get_current_provider(self) -> Optional[AvailableLLMProvider]:
-        current_provider = self.config_service.load_config().current_llm_provider
-        if current_provider is None:
-            return None
-        return current_provider
+    def get_current_llm_provider(self) -> Optional[AvailableLLMProvider]:
+        return self.current_llm_provider
 
     def get_supported_llm_providers(self) -> list[SupportedLLMProvider]:
         return self.config_service.load_config().supported_llm_providers
