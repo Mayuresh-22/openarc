42358fb396248ef120ee88b4ff52657f95597e6c
Mayuresh-22 <mayureshchoudhary22@gmail.com>
2026-02-28 15:51:53 +0530

    Refactor ensure_file_exists utility method to support default_content optional parameter and update docstring

--- src/utils/file.py
+++ src/utils/file.py
@@ -1,14 +1,16 @@
 import os
 
 
-def ensure_file_exists(path: str) -> None:
+def ensure_file_exists(path: str, default_content: str = "") -> None:
     """
-    Ensures that a file exists at the specified path. If the file does not exist, it creates an empty file.
+    Ensures that a file exists at the specified path. If the file does not exist, it creates an new file with default content.
 
     :param path: The path to the file to check or create
     :type path: str
+    :param default_content: The content to write to the file if it does not exist, defaults to an empty string
+    :type default_content: str, optional
     """
     os.makedirs(os.path.dirname(path), exist_ok=True)
     if not os.path.exists(path):
         with open(path, "w") as f:
-            f.write("")
+            f.write(default_content)
