05abc00b831c77d6547844871fcee256d7ec8c63
Mayuresh-22 <mayureshchoudhary22@gmail.com>
2026-02-28 15:48:47 +0530

    Refactor ConfigFileType to include AgentConfig and update default values

--- src/types/config.py
+++ src/types/config.py
@@ -1,4 +1,9 @@
-from typing import Optional
+from agno.models.cerebras import Cerebras
+from agno.models.ollama import Ollama
+from agno.models.groq import Groq
+from agno.models.openai import OpenAIResponses
+
+from typing import Literal, Optional
 from pydantic import BaseModel
 from os.path import dirname as up
 import os
@@ -22,9 +27,20 @@ class CurrentLLMProvider(BaseModel):
     model_id: str
 
 
+class AgentConfig(BaseModel):
+    name: str
+    model: Optional[Cerebras | Ollama | Groq | OpenAIResponses] = None
+    description: str = ""
+    model_config = {"arbitrary_types_allowed": True}
+
+
 class ConfigFileType(BaseModel):
-    available_llm_providers: list[AvailableLLMProvider]
-    available_llm_providers_by_code_name: list[tuple[str, str]]
-    current_llm_provider: Optional[AvailableLLMProvider]
+    agents: dict[Literal["planner", "executor", "verifier"], AgentConfig] = {
+        "planner": AgentConfig(name="Planner Agent"),
+        "executor": AgentConfig(name="Executor Agent"),
+        "verifier": AgentConfig(name="Verifier Agent"),
+    }
+    available_llm_providers: list[AvailableLLMProvider] = []
+    available_llm_providers_by_code_name: list[tuple[str, str]] = []
     root_dir: str = os.path.join(up(up(up(__file__))))
-    supported_llm_providers: list[SupportedLLMProvider]
+    supported_llm_providers: list[SupportedLLMProvider] = []
