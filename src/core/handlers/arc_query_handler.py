c663dc9602b38d029da27b6627d577ed017eb4e3
Mayuresh-22 <mayureshchoudhary22@gmail.com>
2026-03-08 00:01:27 +0530

    Refactor ArcQueryHandler to implement singleton pattern and initialise workflow with simple planner agent runner and handle streamed workflow response

--- src/core/handlers/arc_query_handler.py
+++ src/core/handlers/arc_query_handler.py
@@ -1,7 +1,35 @@
+from agno.workflow import Workflow
+from agno.db.sqlite import SqliteDb
+from typing_extensions import Self
+
+from src.const.workflow import WORKFLOW_MEMORY_PATH
+from src.core.agents.planner.run import run_planner_agent
 from src.core.handlers.base_handler import BaseHandler
 from src.types.cli import CLIOutput
 
 
 class ArcQueryHandler(BaseHandler):
+    _instance = None
+
+    def __new__(cls) -> Self:
+        if cls._instance is None:
+            cls._instance = super(ArcQueryHandler, cls).__new__(cls)
+        return cls._instance
+    
+    def __init__(self):
+        self.workflow = Workflow(
+            name="OpenArc CLI",
+            db=SqliteDb(WORKFLOW_MEMORY_PATH),
+            steps=[run_planner_agent]
+        )
+
     def handle(self, content: list[str]):
+        workflow_response = self.workflow.run(
+            content[0], stream=True
+        )
+        for event in workflow_response:
+            if event.event and event.event != "WorkflowStarted" and event.event != "StepOutputWorkflowCompleted":  # type: ignore
+                print(f"{event.event}", end="", flush=True)  # type: ignore
+        print("\n")
+    
         return CLIOutput(stdout=None, stderr=None, exitcode=0)
