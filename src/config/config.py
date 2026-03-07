1da79300dd43ac997398b2bea780b4b7cbf1c2d2
Mayuresh-22 <mayureshchoudhary22@gmail.com>
2026-03-07 23:51:44 +0530

    Refactor config file path handling to use GLOBAL_PATH consistently

--- src/config/config.py
+++ src/config/config.py
@@ -1,17 +1,19 @@
 import os
 import json
 from os.path import dirname as up
+from pathlib import Path
 
 from src.types.config import ConfigFileType
 from src.utils.file import ensure_file_exists
 
 
 ROOT_PATH = up(up(up(__file__)))
-CONFIG_PATH = os.path.join(ROOT_PATH, ".openarc/config.json")
+CWD_PATH = Path.cwd()
+GLOBAL_PATH = os.path.join(ROOT_PATH, ".openarc/config.json")
 
 # Ensure config file exists at startup
 ensure_file_exists(
-    CONFIG_PATH, default_content=ConfigFileType().model_dump_json(indent=4)
+    GLOBAL_PATH, default_content=ConfigFileType().model_dump_json(indent=4)
 )
 
 
@@ -24,12 +26,12 @@ class ConfigService:
         return cls._instance
 
     def load_config(self) -> ConfigFileType:
-        with open(CONFIG_PATH, "r") as f:
+        with open(GLOBAL_PATH, "r") as f:
             self.config_file = ConfigFileType(**json.load(f))
         return self.config_file
 
     def save_config(self):
-        with open(CONFIG_PATH, "w") as f:
+        with open(GLOBAL_PATH, "w") as f:
             json.dump(self.config_file.model_dump(), f, indent=4)
 
     def update_config(self, new_config: dict):
